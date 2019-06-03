c7f9d43f894be7376ad1add597accc39ebed6c81
erozonachi <erozonachi.james@gmail.com>
2019-06-03 20:08:42 +0100

    Add event handlers to the add comment component
    Added form onSubmit and input onChange event handlers to the add
    comment component.

--- instagram-app/src/components/CommentSection/AddComment.js
+++ instagram-app/src/components/CommentSection/AddComment.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
-export default function AddComment() {
+export default function AddComment(props) {
+  const submitHandler = (event) => {
+    event.preventDefault();
+    props.submitHandler(props.id);
+  };
+
+  const onChangeHandler = (event) => {
+    event.stopPropagation();
+    props.changeHandler(event.target.value);
+  }
   return(
-    <div className='add-comment-container'>
-      <input placeholder='Add a comment...' />
-      <button>&middot;&middot;&middot;</button>
-    </div>
+    <form onSubmit={submitHandler} className='add-comment-container'>
+      <input onChange={onChangeHandler} value={props.val} placeholder='Add a comment...' />
+      <button type='submit'>&middot;&middot;&middot;</button>
+    </form>
   );
 }
