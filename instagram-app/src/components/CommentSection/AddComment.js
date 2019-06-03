3215d0200512117996df8cfc98b844ddf463d1e2
erozonachi <erozonachi.james@gmail.com>
2019-06-03 17:43:06 +0100

    Create add comment component

---
+++ instagram-app/src/components/CommentSection/AddComment.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AddComment() {
+  return(
+    <div className='comment-container'>
+      <input placeholder='Add a comment...' />
+      <button>&middot;&middot;&middot;</button>
+    </div>
+  );
+}
