7e323f087eaae8a833d5c2cd35fb19d455ad5286
erozonachi <erozonachi.james@gmail.com>
2019-06-03 18:06:59 +0100

    Create comment section component

---
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Comment from './Comment';
+import AddComment from './AddComment';
+
+export default function CommentSection(props) {
+  return (
+    <div className='comment-container'>
+      <ul>
+        {props.comments.map(comment => <Comment user={comment.username} text={comment.text} />)}
+      </ul>
+      <span>{props.timestamp}</span>
+      <AddComment />
+    </div>
+  );
+}
