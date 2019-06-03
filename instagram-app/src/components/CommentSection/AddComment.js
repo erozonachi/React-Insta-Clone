dc98378eed6b5a39db7d2aa49460072baa113b75
erozonachi <erozonachi.james@gmail.com>
2019-06-03 18:02:37 +0100

    Modify add comment component
    Change the value of the div className

--- instagram-app/src/components/CommentSection/AddComment.js
+++ instagram-app/src/components/CommentSection/AddComment.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function AddComment() {
   return(
-    <div className='comment-container'>
+    <div className='add-comment-container'>
       <input placeholder='Add a comment...' />
       <button>&middot;&middot;&middot;</button>
     </div>
