4ca2f8d48e407d195a05c7b973918399f9145f52
erozonachi <erozonachi.james@gmail.com>
2019-06-03 23:20:38 +0100

    Check prop types on post container component

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PostHeader from './PostHeader';
 import PostLike from './PostLike';
 import CommentSection from '../CommentSection/CommentSection';
+import PropTypes from 'prop-types';
 import './Posts.css';
 
 export default function PostContainer(props) {
@@ -21,3 +22,18 @@ export default function PostContainer(props) {
     </div>
   );
 }
+
+PostContainer.propTypes = {
+  post: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    thumbnailUrl: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    likes: PropTypes.number.isRequired,
+    newComment: PropTypes.string.isRequired,
+    timestamp: PropTypes.string.isRequired,
+    comments: PropTypes.arrayOf(PropTypes.object),
+  }),
+  changeHandler: PropTypes.func.isRequired,
+  submitHandler: PropTypes.func.isRequired,
+};
