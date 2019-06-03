a44a9d2ca0a4eb7caa16b7259da31bd1308d82b4
erozonachi <erozonachi.james@gmail.com>
2019-06-03 19:11:51 +0100

    Style post like component

--- instagram-app/src/components/PostContainer/PostLike.js
+++ instagram-app/src/components/PostContainer/PostLike.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import FavoriteIcon from '@material-ui/icons/FavoriteBorder';
-import CommentIcon from '@material-ui/icons/AddCommentOutlined';
+import CommentIcon from '@material-ui/icons/ModeCommentOutlined';
 
 export default function PostLike(props) {
   return(
     <div className='like-container'>
       <div className='like-controls'>
-        <FavoriteIcon />
-        <CommentIcon />
+        <FavoriteIcon className='like-control' />
+        <CommentIcon className='like-control' />
       </div>
       <span>{`${props.likes} ${props.likes > 1? 'likes' : 'like'}`}</span>
     </div>
