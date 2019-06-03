e09e796d20d51f8690dd0a3f53bc9f8534c8d9ff
erozonachi <erozonachi.james@gmail.com>
2019-06-03 19:22:29 +0100

    Style the comment component

--- instagram-app/src/components/CommentSection/CommentSection.js
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Comment from './Comment';
 import AddComment from './AddComment';
+import './Comments.css';
 
 export default function CommentSection(props) {
   return (
