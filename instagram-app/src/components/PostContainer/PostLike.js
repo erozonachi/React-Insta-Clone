85d03eb71f24f38f83f6207468441c714c57c59b
erozonachi <erozonachi.james@gmail.com>
2019-06-04 15:51:29 +0100

    Style material icons
    Styled material icons used in SearchBar and PostLike components.

--- instagram-app/src/components/PostContainer/PostLike.js
+++ instagram-app/src/components/PostContainer/PostLike.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import FavoriteIcon from '@material-ui/icons/FavoriteBorder';
 import CommentIcon from '@material-ui/icons/ModeCommentOutlined';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  icon: {
+    fontSize: 30,
+  },
+  iconSpaced: {
+    margin: theme.spacing(.3),
+    fontSize: 30,
+  },
+}));
 
 export default function PostLike(props) {
   return(
     <div className='like-container'>
       <div className='like-controls'>
-        <FavoriteIcon className='like-control' />
-        <CommentIcon className='like-control' />
+        <FavoriteIcon className={`${useStyles().iconSpaced} like-control`} />
+        <CommentIcon className={`${useStyles().iconSpaced} like-control`} />
       </div>
       <span>{`${props.likes} ${props.likes > 1? 'likes' : 'like'}`}</span>
     </div>
--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -3,14 +3,25 @@ import PhotoCamera from '@material-ui/icons/PhotoCameraOutlined';
 import ExploreIcon from '@material-ui/icons/ExploreOutlined';
 import FavoriteIcon from '@material-ui/icons/FavoriteBorder';
 import PersonIcon from '@material-ui/icons/PersonOutlineOutlined';
+import { makeStyles } from '@material-ui/core/styles';
 import './SearchBar.css';
+
+const useStyles = makeStyles(theme => ({
+  icon: {
+    fontSize: 30,
+  },
+  iconSpaced: {
+    margin: theme.spacing(.5),
+    fontSize: 30,
+  },
+}));
 
 export default function SearchBar() {
   return(
     <div className='search-bar'>
       <div className='logo-container'>
         <span>
-          <PhotoCamera className='logo-item' />
+          <PhotoCamera className={`${useStyles().icon} logo-item`} />
         </span>
         <h1 className='logo-item'>
           Instagram
@@ -20,9 +31,9 @@ export default function SearchBar() {
         <input className='search-box' placeholder='Search' />
       </div>
       <div className='search-controls'>
-        <ExploreIcon className='control' />
-        <FavoriteIcon className='control' />
-        <PersonIcon className='control' />
+        <ExploreIcon className={`${useStyles().iconSpaced} control`} />
+        <FavoriteIcon className={`${useStyles().iconSpaced} control`} />
+        <PersonIcon className={`${useStyles().iconSpaced} control`} />
       </div>
     </div>
   );
