f5cf1c5700445997e5cab1d5dd59cf1b92b5b80a
erozonachi <erozonachi.james@gmail.com>
2019-06-05 14:17:20 +0100

    Render a basic login form

---
+++ instagram-app/src/components/Login/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class Login extends React.Component {
+
+  render() {
+    return (
+      <div className='login-container'>
+        <h1>Instagram</h1>
+        <form>
+          <input />
+          <input />
+          <button type='submit'>Login</button>
+        </form>
+      </div>
+    );
+  }
+}
