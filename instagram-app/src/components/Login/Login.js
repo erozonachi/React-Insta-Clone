b11c5cfd43619fac307bbee04a1269cecb2528fe
erozonachi <erozonachi.james@gmail.com>
2019-06-05 14:22:57 +0100

    Set up state for login details on login component

--- instagram-app/src/components/Login/Login.js
+++ instagram-app/src/components/Login/Login.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
 export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    }
+  }
 
   render() {
     return (
       <div className='login-container'>
         <h1>Instagram</h1>
         <form>
-          <input />
-          <input />
+          <input value={this.state.username} placeholder='Enter Username' />
+          <input value={this.state.password} type='password' placeholder='Enter Password' />
           <button type='submit'>Login</button>
         </form>
       </div>
